213363e14ab2a04ef0b6d917b87e31005dc11dff
vy trần <trancongvy@gmail.com>
2021-04-28 09:10:04 +0700

    Add project files.

---
+++ Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BPMAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BPMAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            // Connection x = new Connection();
+          //  ViewBag.Title = Connection.GetConnection("CBASGD200");
+
+            return View();
+        }
+    }
+}
